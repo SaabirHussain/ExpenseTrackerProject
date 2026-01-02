d6329400629a79204f9e0ceaabfcff0390b5a0e2
SaabirHussain <anwarsaabir@gmail.com>
2026-01-01 20:20:20 -0500

    Initial Commit

---
+++ MyExpense/MyExpenseApp.swift
@@ -0,0 +1,20 @@
+//
+//  MyExpenseApp.swift
+//  MyExpense
+//
+//  Created by Saabir Hussain on 2026-01-01.
+//
+
+import SwiftUI
+
+@main
+struct MyExpenseApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ MyExpenseTests/MyExpenseTests.swift
@@ -0,0 +1,17 @@
+//
+//  MyExpenseTests.swift
+//  MyExpenseTests
+//
+//  Created by Saabir Hussain on 2026-01-01.
+//
+
+import Testing
+@testable import MyExpense
+
+struct MyExpenseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
